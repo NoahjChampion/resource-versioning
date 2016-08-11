2ff6a978c25533568d0ab3e6ec48e204a7fc1a08
Noahj Champion <NoahjChampion@users.noreply.github.com>
2016-08-11 13:59:18 -0400

    Added auto insert Apache settings
    Added code to detect on Apache if the .htaccess is writable. If it is then auto insert the needed Apache rules. If not, notify the user.
    
    Notice: FOR NGINX USERS
    I'm not sure how to detect if the hosting env is Nginx. So if the if the .htaccess isn't writeable the plugin will always self deactivate itself. This means currently with Nginx users the plugin will fail everytime. 
    
    Maybe you can adjust it to work for both envs out of the box?

--- revving.php
+++ revving.php
@@ -1,15 +1,90 @@
 <?php
 /*
-Plugin Name: Resource Versioning
-Description: Turn Query String Parameters into file revision numbers.
-Version: 0.1.2
-Author: Viktor Szépe
-License: GNU General Public License (GPL) version 2
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-GitHub Plugin URI: https://github.com/szepeviktor/resource-versioning
-Options: O1_REMOVE_ALL_QARGS
+	Plugin Name: Resource Versioning
+	Description: Turn Query String Parameters into file revision numbers.
+	Version: 0.1.2
+	Author: Viktor Szépe
+	License: GNU General Public License (GPL) version 2
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+	GitHub Plugin URI: https://github.com/szepeviktor/resource-versioning
+	Options: O1_REMOVE_ALL_QARGS
 */
-
+// ----------------------------------------------------------------------------------------------------
+// Register Activation Hook
+// ----------------------------------------------------------------------------------------------------
+register_activation_hook(__FILE__, 'ResourceVersioning_activation_hook');
+// ----------------------------------------------------------------------------------------------------
+// If the user's Htaccess file is not editable...
+//  Check if the htaccess file is writeable and if not deactivate and give the admin a notice that their .htaccess is not writeable.
+// ----------------------------------------------------------------------------------------------------
+$htaccess_file = ABSPATH . '.htaccess';
+if (!is_writeable($htaccess_file)) {
+	// ----------------------------------------------------------------------------------------------------
+	// Only run on our needed page (ie: the plugins page)
+	// ----------------------------------------------------------------------------------------------------
+	global $current_screen;
+	$base = $current_screen->base;
+	$id = $current_screen->id;
+	if ($base == 'plugins' || $id == 'plugins') {
+		add_action('admin_notices', 'ResourceVersioning_is_writable_htaccess_admin_notice');
+		return;
+	}
+}
+function ResourceVersioning_is_writable_htaccess_admin_notice() {
+	// ----------------------------------------------------------------------------------------------------
+	// Only run on our needed page (ie: the plugins page)
+	// ----------------------------------------------------------------------------------------------------
+	global $current_screen;
+	$base = $current_screen->base;
+	$id = $current_screen->id;
+	if ($base == 'plugins' || $id == 'plugins') {
+		echo "<div class='error'>File Query Converter to File Versions requires your .htaccess file to be writeable. Please make the file writeable bt changing it FILE PERMISSIONS or find out why your .htaccess file is not writeable, and reactivate the plugin (File Query Converter to File Versions...). Apache Superman has been deactivated in the meantime. Please contact your hosting provider about this or reach out to us for support @ http://custom-theme.com</div>";
+	}
+}
+// ----------------------------------------------------------------------------------------------------
+// Htaccess File Edits
+// -- If the .htaccess file is writable then proceed to insert our .htaccess file edits
+// ----------------------------------------------------------------------------------------------------
+function ResourceVersioning_activation_hook() {
+	// ----------------------------------------------------------------------------------------------------
+	// Only run on our needed page (ie: the plugins page)
+	// ----------------------------------------------------------------------------------------------------
+	global $current_screen;
+	$base = $current_screen->base;
+	$id = $current_screen->id;
+	if ($base == 'plugins' || $id == 'plugins') {
+		// ----------------------------------------------------------------------------------------------------
+		// If the htaccess file is not writeable, return
+		// ----------------------------------------------------------------------------------------------------
+		$htaccess_file = ABSPATH . '.htaccess';
+		if (!is_writeable($htaccess_file)) {
+			return;
+		}
+		// ----------------------------------------------------------------------------------------------------
+		// If the function doesn't exist, require it
+		// ----------------------------------------------------------------------------------------------------
+		if (!function_exists('insert_with_markers')) {
+			require_once (ABSPATH . 'wp-admin/includes/misc.php');
+		}
+		// ----------------------------------------------------------------------------------------------------
+		// Insert our Apache Superman .htaccess file content
+		// ----------------------------------------------------------------------------------------------------
+		$insertion = '
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^(.+)\.\d\d+\.(js|css|png|jpg|jpeg|gif|ico)$ $1.$2 [NC,L]
+';
+		// ----------------------------------------------------------------------------------------------------
+		// Explode, because we are using an array
+		// ----------------------------------------------------------------------------------------------------
+		$insertion = explode("\n", $insertion);
+		insert_with_markers($htaccess_file, '> File Query Converter to File Versions.', $insertion);
+	}
+}
+// ----------------------------------------------------------------------------------------------------
+// @ViktorSzépe Code
+// ----------------------------------------------------------------------------------------------------
 /**
  * Trigger fail2ban
  */
@@ -25,13 +100,11 @@ if ( ! function_exists( 'add_filter' ) ) {
     }
     exit;
 }
-
 /**
  * Filter script and style source URL-s.
  */
-add_filter( 'script_loader_src', 'o1_src_revving' );
-add_filter( 'style_loader_src', 'o1_src_revving' );
-
+add_filter( 'script_loader_src', 'ResourceVersioning_src' );
+add_filter( 'style_loader_src', 'ResourceVersioning_src' );
 /**
  * Insert version into filename from query string.
  *
@@ -39,45 +112,38 @@ add_filter( 'style_loader_src', 'o1_src_revving' );
  *
  * @return string      Versioned URL
  */
-function o1_src_revving( $src ) {
-
+function ResourceVersioning_src( $src ) {
     // Check for external or admin URL
     $siteurl_noscheme = str_replace( array( 'http:', 'https:' ), '', site_url() );
     $contenturl_noscheme = str_replace( array( 'http:', 'https:' ), '', WP_CONTENT_URL );
     if ( is_admin()
-        || ( ! o1_starts_with( $src, site_url() )
-            && ! o1_starts_with( $src, $siteurl_noscheme )
-            && ! o1_starts_with( $src, WP_CONTENT_URL )
-            && ! o1_starts_with( $src, $contenturl_noscheme )
+        || ( ! ResourceVersioning_start( $src, site_url() )
+            && ! ResourceVersioning_start( $src, $siteurl_noscheme )
+            && ! ResourceVersioning_start( $src, WP_CONTENT_URL )
+            && ! ResourceVersioning_start( $src, $contenturl_noscheme )
         )
     ) {
         return $src;
     }
-
     // Separate query string from the URL
     $parts = preg_split( '/\?/', $src, 2 );
-
     // Find version in query
     parse_str( $parts[1], $kwargs );
     if ( empty( $kwargs['ver'] ) ) {
         return $src;
     }
-
     // Sanitize version
     $ver = preg_replace( '/[^0-9]/', '', $kwargs['ver'] );
     // We need at least two digits for the rewrite rule
     if ( strlen( $ver ) < 2 ) {
         $ver = '0' . $ver;
     }
-
     // Find where to insert version
     $pos = strrpos( $parts[0], '.' );
-
     // No dot in URL
     if ( false === $pos ) {
         return $src;
     }
-
     // Remove all query arguments
     if ( defined( 'O1_REMOVE_ALL_QARGS' ) && O1_REMOVE_ALL_QARGS ) {
         $new_query = '';
@@ -85,7 +151,6 @@ function o1_src_revving( $src ) {
         unset( $kwargs['ver'] );
         $new_query = build_query( $kwargs );
     }
-
     // Return the new URL
     return sprintf( '%s.%s.%s%s',
         substr( $parts[0], 0, $pos ),
@@ -94,7 +159,6 @@ function o1_src_revving( $src ) {
         $new_query ? '?' . $new_query : ''
     );
 }
-
 /**
  * Return if haystack starts with needle.
  *
@@ -103,8 +167,54 @@ function o1_src_revving( $src ) {
  *
  * @return boolean Whether starts with or not.
  */
-function o1_starts_with( $haystack, $needle ) {
-
+function ResourceVersioning_start( $haystack, $needle ) {
      $length = strlen( $needle );
      return ( $needle === substr( $haystack, 0, $length ) );
 }
+// ----------------------------------------------------------------------------------------------------
+// Register Deactivation Hook
+// ----------------------------------------------------------------------------------------------------
+register_deactivation_hook(__FILE__, 'ResourceVersioning_deactivation_hook');
+// ----------------------------------------------------------------------------------------------------
+// Deactivation Cleanup
+// -- If we are being deactivated then proceed to remove our .htaccess file edits
+// ----------------------------------------------------------------------------------------------------
+function ResourceVersioning_deactivation_hook() {
+	// ----------------------------------------------------------------------------------------------------
+	// Only run on our needed page (ie: the plugins page)
+	// ----------------------------------------------------------------------------------------------------
+	global $current_screen;
+	$base = $current_screen->base;
+	$id = $current_screen->id;
+	if ($base == 'plugins' || $id == 'plugins') {
+		// ----------------------------------------------------------------------------------------------------
+		// If htaccess file is not writeable, return
+		// ----------------------------------------------------------------------------------------------------
+		$htaccess_file = ABSPATH . '.htaccess';
+		if (!is_writeable($htaccess_file)) {
+			return;
+		}
+		// ----------------------------------------------------------------------------------------------------
+		// If the function doesn't exist, require it
+		// ----------------------------------------------------------------------------------------------------
+		if (!function_exists('insert_with_markers')) {
+			require_once (ABSPATH . 'wp-admin/includes/misc.php');
+		}
+		// ----------------------------------------------------------------------------------------------------
+		// Remove the rows
+		// ----------------------------------------------------------------------------------------------------
+		insert_with_markers($htaccess_file, '> File Query Converter to File Versions.', '');
+	}
+}
+// ======================================================================================================================================================
+//
+if ( ! function_exists( 'add_action' ) ) { header( 'Status: 403 Forbidden' ); header( 'HTTP/1.1 403 Forbidden' ); exit(); }
+//
+// --------------- This serves as a security enhancement for when hackers may attempt to scan the directory of this file.
+// --------------- This will give the hacker the wrong Header Status, hopefully misleading the hacker
+// --------------- to think that this directory is blocked by the hosting itself (which may not be true) 
+// --------------- So hopefully the hacker leaves this directory alone because they will think they need to enter through the hosting account 
+// --------------- to achieve their hacking desire. But in reality, we just lied to them.
+// --------------- Is this lying a good thing? Will good karma return to us under this lie? I concur. It will. :)
+//
+// ======================================================================================================================================================
